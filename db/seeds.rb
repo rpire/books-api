d46e5ee78255dfeaf465e9cbcf9607a883572477
rpire <rubenpire7@gmail.com>
2022-01-29 16:09:28 -0400

    Update the seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: 'Edna López', bio: 'I love technical books.')
+User.create(name: 'Rubén Pire', bio: 'I love magic realism books.')
+
+Book.create(
+  title: 'One Hundred Years of Solitude',
+  author: 'Gabriel García Márquez',
+  num_of_chapters: 30,
+  current_chapter: 11,
+  category: 'Magic Realism',
+  user_id: 2
+)
+
+Book.create(
+  title: 'Electrical Machines',
+  author: 'Stephen J. Chapman',
+  num_of_chapters: 48,
+  current_chapter: 2,
+  category: 'Engineering',
+  user_id: 1
+)
+
+Book.create(
+  title: 'Twelve Pilgrim Tales',
+  author: 'Gabriel García Márquez',
+  num_of_chapters: 15,
+  current_chapter: 7,
+  category: 'Magic Realism',
+  user_id: 2
+)
