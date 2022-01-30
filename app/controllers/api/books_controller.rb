456ae49eccf417f2034e3a4411284ce4bf2f3bbc
rpire <rubenpire7@gmail.com>
2022-01-30 16:11:46 -0400

    Change index action to display only the current user's books

--- app/controllers/api/books_controller.rb
+++ app/controllers/api/books_controller.rb
@@ -2,7 +2,7 @@ class Api::BooksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @books = Book.all
+    @books = current_user.books
 
     render json: @books
   end
