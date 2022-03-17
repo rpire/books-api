035a1a270ecfa188750f93cf6448b48f100ed4e5
rpire <rubenpire7@gmail.com>
2022-03-16 23:30:58 -0400

    Add a devise sanitizer for user's name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,12 @@ class ApplicationController < ActionController::API
       ]
     }, status: :bad_request
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+  end
 end
