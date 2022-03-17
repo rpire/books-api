2cdd9bc5a5ffbd732b4fc7e5534e487cbe57da16
rpire <rubenpire7@gmail.com>
2022-03-16 23:28:20 -0400

    Run pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_230657) do
+ActiveRecord::Schema.define(version: 2022_03_17_002929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_03_15_230657) do
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
     t.string "role", default: "user"
+    t.integer "icon", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
