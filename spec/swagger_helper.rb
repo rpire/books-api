9b72e830836aa654bedc39b81ce6325c9b91363a
rpire <rubenpire7@gmail.com>
2022-03-19 15:59:03 -0400

    Add component schemas in the swagger_helper file

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+# rubocop:disable Metrics
+
 RSpec.configure do |config|
   config.swagger_root = Rails.root.join('swagger').to_s
   config.swagger_docs = {
@@ -7,12 +9,45 @@ RSpec.configure do |config|
       openapi: '3.0.1',
       info: { title: 'API V1', version: 'v1' },
       components: {
-        securitySchemes: {
-          bearer_auth: {
-            type: :http,
-            scheme: :bearer,
-            bearerFormat: JWT
+        schemas: {
+          registration: {
+            type: :object,
+            properties: {
+              user: {
+                name: :string,
+                email: :string,
+                password: :string
+              }
+            },
+            required: %w[name email password]
+          },
+          session: {
+            type: :object,
+            properties: {
+              user: {
+                email: :string,
+                password: :string
+              }
+            },
+            required: %w[email password]
+          },
+          book: {
+            type: :object,
+            properties: {
+              book: {
+                title: :string,
+                author: :string,
+                category: :string,
+                current_chapter: :string,
+                num_of_pages: :integer,
+                current_page: :integer
+              }
+            },
+            required: %w[title author category current_chapter num_of_pages current_page]
           }
+        },
+        securitySchemes: {
+          bearer_auth: { type: :http, scheme: :bearer, bearerFormat: JWT }
         }
       },
       paths: {},
@@ -20,9 +55,7 @@ RSpec.configure do |config|
         {
           url: 'https://{defaultHost}',
           variables: {
-            defaultHost: {
-              default: 'www.example.com'
-            }
+            defaultHost: { default: 'www.example.com' }
           }
         }
       ]
@@ -30,3 +63,5 @@ RSpec.configure do |config|
   }
   config.swagger_format = :yaml
 end
+
+# rubocop:enable Metrics
