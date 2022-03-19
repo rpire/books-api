e6d744fbfbb5cf9fe43535328a22a811255c8a1b
rpire <rubenpire7@gmail.com>
2022-03-18 21:19:55 -0400

    Update the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8 }
   validates :bio, length: { maximum: 250 }
   validates :icon, presence: true,
                    numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 9 }
