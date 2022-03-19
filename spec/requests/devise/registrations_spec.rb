f14109bc5dc26fdf532a450ba0cf7dc6e9bbaab2
rpire <rubenpire7@gmail.com>
2022-03-18 21:21:47 -0400

    Add request tests for the registrations controller

---
+++ spec/requests/devise/registrations_spec.rb
@@ -0,0 +1,40 @@
+require 'swagger_helper'
+
+RSpec.describe 'devise/registrations', type: :request do
+  path '/api/signup' do
+    post('register user') do
+      tags 'User Registration'
+      consumes 'application/json'
+      parameter name: :registration, in: :body, schema: {
+        type: :object,
+        properties: {
+          user: {
+            name: :string,
+            email: :string,
+            password: :string
+          }
+        },
+        required: [:name, :email, :password]
+      }
+      response(200, 'successful') do
+        let(:registration) {
+          {
+            user: {
+              name: 'Ruben',
+              email: 'rpire@email.com',
+              password: '12345678'
+            }
+          }
+        }
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
